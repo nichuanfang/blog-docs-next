6ef41389e1fdbe6e26af34fdd3a854b06c11bc02
nichuanfang <f18326186224@gmail.com>
2024-07-12 21:48:16 +0800

    1

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,51 +1,51 @@
 import { navbar } from 'vuepress-theme-hope'
 
 export default navbar([
-  '/',
+  "/",
   {
-    text: '简历',
-    icon: 'laptop-code',
-    link: '/resume/README.md',
+    text: "简历",
+    icon: "laptop-code",
+    link: "/resume/README.md",
   },
   {
-    text: '面试指南',
-    icon: 'lightbulb',
-    link: '/interview/README.md',
+    text: "面试指南",
+    icon: "lightbulb",
+    link: "/interview/README.md",
   },
   {
-    text: '面试题精简版',
-    icon: 'laptop-code',
-    link: '/面试题&精简版/面试题&精简版.md',
+    text: "面试题精简版",
+    icon: "laptop-code",
+    link: "https://blog.csdn.net/weixin_46603441/article/details/131891932",
   },
   {
-    text: '配置指南',
-    icon: 'laptop-code',
-    link: '/config/README.md',
+    text: "配置指南",
+    icon: "laptop-code",
+    link: "/config/README.md",
   },
   {
-    text: '项目',
-    icon: 'lightbulb',
+    text: "项目",
+    icon: "lightbulb",
     children: [
       {
-        text: 'movie-tvshow-spider',
-        icon: 'lightbulb',
-        link: 'https://github.com/nichuanfang/movie-tvshow-spider',
+        text: "movie-tvshow-spider",
+        icon: "lightbulb",
+        link: "https://github.com/nichuanfang/movie-tvshow-spider",
       },
       {
-        text: 'music-tool-kit',
-        icon: 'lightbulb',
-        link: 'https://github.com/nichuanfang/music-tool-kit',
+        text: "music-tool-kit",
+        icon: "lightbulb",
+        link: "https://github.com/nichuanfang/music-tool-kit",
       },
       {
-        text: 'tgbot',
-        icon: 'lightbulb',
-        link: 'https://github.com/nichuanfang/tgbot',
+        text: "tgbot",
+        icon: "lightbulb",
+        link: "https://github.com/nichuanfang/tgbot",
       },
     ],
   },
   {
-    text: '个人博客',
-    icon: 'book',
-    link: 'https://blog.jaychou.site',
+    text: "个人博客",
+    icon: "book",
+    link: "https://blog.jaychou.site",
   },
-])
+]);
